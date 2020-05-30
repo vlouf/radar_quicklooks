ef399d050d4f4c748bd37fbafb02380d03441971
Valentin Louf <valentin.louf@bom.gov.au>
2020-05-30 16:07:29 +1000

    MODIF: pep8 formatting

--- quicklooks/quicklooks.py
+++ quicklooks/quicklooks.py
@@ -28,13 +28,23 @@ def _adjust_csu_scheme_colorbar_for_pyart(cb):
     """
     Generate colorbar for the hydrometeor classification.
     """
-    cb.set_ticks(np.arange(.55, 11, .9))
-    cb.ax.set_yticklabels(['None','Drizzle','Rain',
-                           'Ice Crystals','Aggregates','Wet/Melting Snow','Vertically Aligned Ice',
-                           'Low-Density Graupel','High-Density Graupel',
-                           'Hail',
-                           'Big Drops'])
-    cb.ax.set_ylabel('')
+    cb.set_ticks(np.arange(0.55, 11, 0.9))
+    cb.ax.set_yticklabels(
+        [
+            "None",
+            "Drizzle",
+            "Rain",
+            "Ice Crystals",
+            "Aggregates",
+            "Wet/Melting Snow",
+            "Vertically Aligned Ice",
+            "Low-Density Graupel",
+            "High-Density Graupel",
+            "Hail",
+            "Big Drops",
+        ]
+    )
+    cb.ax.set_ylabel("")
     cb.ax.tick_params(length=0)
     return cb
 
@@ -53,13 +63,13 @@ def plot_quicklook(input_file: str, figure_path: str):
     try:
         radar = pyart.io.read(input_file)
     except Exception:
-        print(f'Could not process file {input_file}')
+        print(f"Could not process file {input_file}")
         traceback.print_exc()
         return None
 
     gatefilter = pyart.filters.GateFilter(radar)
-    gatefilter.exclude_invalid('corrected_reflectivity')
-    radar_date = cftime.num2pydate(radar.time['data'][0], radar.time['units'])
+    gatefilter.exclude_invalid("corrected_reflectivity")
+    radar_date = cftime.num2pydate(radar.time["data"][0], radar.time["units"])
 
     if figure_path is None:
         return None
@@ -91,73 +101,140 @@ def plot_quicklook(input_file: str, figure_path: str):
     the_ax = the_ax.flatten()
     # Plotting reflectivity
 
-    gr.plot_ppi('corrected_reflectivity', ax=the_ax[0], gatefilter=gatefilter, cmap='pyart_NWSRef')
-    the_ax[0].set_title(gr.generate_title('corrected_reflectivity', sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
+    gr.plot_ppi(
+        "corrected_reflectivity",
+        ax=the_ax[0],
+        gatefilter=gatefilter,
+        cmap="pyart_NWSRef",
+    )
+    the_ax[0].set_title(
+        gr.generate_title(
+            "corrected_reflectivity", sweep=0, datetime_format="%Y-%m-%dT%H:%M"
+        )
+    )
 
-    #echo classification
-    #create colormap
-    hca_colors = ['White',
-                  'LightBlue',
-                  'SteelBlue',
-                  'MediumBlue',
-                  'Plum',
-                  'MediumPurple',
-                  'm',
-                  'Green',
-                  'YellowGreen',
-                  'Gold',
-                  'Red']
+    # echo classification
+    # create colormap
+    hca_colors = [
+        "White",
+        "LightBlue",
+        "SteelBlue",
+        "MediumBlue",
+        "Plum",
+        "MediumPurple",
+        "m",
+        "Green",
+        "YellowGreen",
+        "Gold",
+        "Red",
+    ]
     hca_cmap = colors.ListedColormap(hca_colors)
-    gr.plot_ppi('radar_echo_classification', ax=the_ax[1], cmap=hca_cmap, vmin=0, vmax=10)
-    the_ax[1].set_title(gr.generate_title('radar_echo_classification', sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
-    #adjust colorbar for classification
+    gr.plot_ppi(
+        "radar_echo_classification", ax=the_ax[1], cmap=hca_cmap, vmin=0, vmax=10
+    )
+    the_ax[1].set_title(
+        gr.generate_title(
+            "radar_echo_classification", sweep=0, datetime_format="%Y-%m-%dT%H:%M"
+        )
+    )
+    # adjust colorbar for classification
     gr.cbs[1] = _adjust_csu_scheme_colorbar_for_pyart(gr.cbs[1])
 
-    gr.plot_ppi('radar_estimated_rain_rate', ax=the_ax[2])
-    the_ax[2].set_title(gr.generate_title('radar_estimated_rain_rate', sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
+    gr.plot_ppi("radar_estimated_rain_rate", ax=the_ax[2])
+    the_ax[2].set_title(
+        gr.generate_title(
+            "radar_estimated_rain_rate", sweep=0, datetime_format="%Y-%m-%dT%H:%M"
+        )
+    )
 
-    gr.plot_ppi('corrected_differential_reflectivity', ax=the_ax[3], gatefilter=gatefilter)
-    the_ax[3].set_title(gr.generate_title('corrected_differential_reflectivity',
-                                          sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
+    gr.plot_ppi(
+        "corrected_differential_reflectivity", ax=the_ax[3], gatefilter=gatefilter
+    )
+    the_ax[3].set_title(
+        gr.generate_title(
+            "corrected_differential_reflectivity",
+            sweep=0,
+            datetime_format="%Y-%m-%dT%H:%M",
+        )
+    )
 
     try:
-        gr.plot_ppi('corrected_differential_phase', ax=the_ax[4], vmin=-180, vmax=180, cmap='pyart_Wild25', gatefilter=gatefilter)
-        the_ax[4].set_title(gr.generate_title('corrected_differential_phase', sweep=0,
-                                              datetime_format='%Y-%m-%dT%H:%M'))
+        gr.plot_ppi(
+            "corrected_differential_phase",
+            ax=the_ax[4],
+            vmin=-180,
+            vmax=180,
+            cmap="pyart_Wild25",
+            gatefilter=gatefilter,
+        )
+        the_ax[4].set_title(
+            gr.generate_title(
+                "corrected_differential_phase",
+                sweep=0,
+                datetime_format="%Y-%m-%dT%H:%M",
+            )
+        )
     except KeyError:
         print(crayons.red("Problem with 'corrected_differential_phase' field."))
         pass
 
     try:
-        gr.plot_ppi('corrected_specific_differential_phase', ax=the_ax[5], vmin=-2, vmax=5,
-                    cmap='pyart_Theodore16', gatefilter=gatefilter)
-        the_ax[5].set_title(gr.generate_title('corrected_specific_differential_phase', sweep=0,
-                                              datetime_format='%Y-%m-%dT%H:%M'))
+        gr.plot_ppi(
+            "corrected_specific_differential_phase",
+            ax=the_ax[5],
+            vmin=-2,
+            vmax=5,
+            cmap="pyart_Theodore16",
+            gatefilter=gatefilter,
+        )
+        the_ax[5].set_title(
+            gr.generate_title(
+                "corrected_specific_differential_phase",
+                sweep=0,
+                datetime_format="%Y-%m-%dT%H:%M",
+            )
+        )
     except KeyError:
-        print(crayons.red("Problem with 'corrected_specific_differential_phase' field."))
+        print(
+            crayons.red("Problem with 'corrected_specific_differential_phase' field.")
+        )
         pass
 
     try:
-        gr.plot_ppi('velocity', ax=the_ax[6], cmap='pyart_NWSVel', vmin=-30, vmax=30)
-        the_ax[6].set_title(gr.generate_title('velocity', sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
+        gr.plot_ppi("velocity", ax=the_ax[6], cmap="pyart_NWSVel", vmin=-30, vmax=30)
+        the_ax[6].set_title(
+            gr.generate_title("velocity", sweep=0, datetime_format="%Y-%m-%dT%H:%M")
+        )
     except KeyError:
         print(crayons.red("Problem with 'raw_velocity' field."))
         pass
 
     try:
-        gr.plot_ppi('corrected_velocity', ax=the_ax[7], gatefilter=gatefilter,
-                    cmap='pyart_NWSVel', vmin=-30, vmax=30)
-        the_ax[7].set_title(gr.generate_title('UNRAVEL_velocity', sweep=0,
-                                              datetime_format='%Y-%m-%dT%H:%M'))
+        gr.plot_ppi(
+            "corrected_velocity",
+            ax=the_ax[7],
+            gatefilter=gatefilter,
+            cmap="pyart_NWSVel",
+            vmin=-30,
+            vmax=30,
+        )
+        the_ax[7].set_title(
+            gr.generate_title(
+                "UNRAVEL_velocity", sweep=0, datetime_format="%Y-%m-%dT%H:%M"
+            )
+        )
     except KeyError as error:
         print(error)
         print(crayons.red("Problem with 'velocity' field."))
         pass
 
     try:
-        gr.plot_ppi('cross_correlation_ratio', ax=the_ax[8], vmin=0.5, vmax=1.05)
-        the_ax[8].set_title(gr.generate_title('cross_correlation_ratio',
-                                              sweep=0, datetime_format='%Y-%m-%dT%H:%M'))
+        gr.plot_ppi("cross_correlation_ratio", ax=the_ax[8], vmin=0.5, vmax=1.05)
+        the_ax[8].set_title(
+            gr.generate_title(
+                "cross_correlation_ratio", sweep=0, datetime_format="%Y-%m-%dT%H:%M"
+            )
+        )
     except KeyError:
         print(crayons.red("Problem with 'cross_correlation_ratio' field."))
         pass
